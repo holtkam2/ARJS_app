6ae7aec3bcd1eaaa19321378c283154e490ae532
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2017-09-29 17:04:55 -0500

    now sends an event back if the index has been requested

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8080;
+var readyToShowTarget = false;
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
+  readyToShowTarget = true;
   res.render('index');
+});
+
+app.get('/checkifready', function(req, res) {
+  res.send(JSON.stringify(readyToShowTarget))
 });
 
 app.listen(port, function() {
