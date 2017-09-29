5cf80e6f01acb6bba0642d3ac6ecf7aeb60e4597
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2017-09-29 13:17:51 -0500

    added a drone model

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var fs = require('fs');
 
 var port = process.env.PORT || 8080;
 
@@ -9,6 +11,11 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/buster_drone/scene.gtlf', function(req, res) {
+  console.log('got a request for the drone file')
+  res.sendFile(__dirname + '/buster_drone/scene.gtlf');
 });
 
 app.listen(port, function() {
