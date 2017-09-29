c685ea6a4c0aa7e296355dc0b5e25b9f1ab694bf
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2017-09-29 13:23:19 -0500

    Revert "added a drone model"
    This reverts commit 5cf80e6f01acb6bba0642d3ac6ecf7aeb60e4597.

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var request = require('request');
-var fs = require('fs');
 
 var port = process.env.PORT || 8080;
 
@@ -11,11 +9,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('index');
-});
-
-app.get('/buster_drone/scene.gtlf', function(req, res) {
-  console.log('got a request for the drone file')
-  res.sendFile(__dirname + '/buster_drone/scene.gtlf');
 });
 
 app.listen(port, function() {
